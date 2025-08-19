737380834ddb8e77456f4301766ea31910327180
alexbgh1 <alex.garnica@alumnos.uach.cl>
2025-08-19 01:00:20 -0400

    Auto-generated commit

--- src/app/shared/components/color-palette/color-palette.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-color-palette',
-  imports: [],
-  templateUrl: './color-palette.component.html',
-})
-export class ColorPaletteComponent {}
