b9e8dd0a80938a99fa8fd191d62ff2f6881788b2
alexbgh1 <alex.garnica@alumnos.uach.cl>
2026-06-23 19:53:10 -0400

    [fix]: landing w/signals transition header

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  signal,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ArrowUpRightIconComponent } from '@shared/icons/arrow-up-right.component';
 import { NAV_LINKS_OBJECTS } from '@shared/constants/nav-links.constants';
@@ -21,13 +27,15 @@ interface Tool {
 })
 export class LandingComponent implements OnInit {
   activeSection = 0;
-  isVisible = false;
+  isVisible = signal(false);
 
   NAV_LINKS_OBJECTS = NAV_LINKS_OBJECTS;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
   ngOnInit(): void {
     setTimeout(() => {
-      this.isVisible = true;
+      this.isVisible.set(true);
     }, 100);
   }
 }
